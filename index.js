630b7bcfa375521ae47e2f03be32a7829ff0303c
longnphan <67768035+longnphan@users.noreply.github.com>
2020-07-11 13:36:56 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const outcomeOptions = ["heads", "tails"];
 var playerScore = 0;
 var computerScore = 0;
 
+/// When player selects heads
 function heads() {
+  document.querySelector(".coin-outcome").innerHTML = "";
   document.querySelector("#coin-flip").classList.add("animate");
   let x = setInterval(randomOutcome, 2000);
-  
+
   function randomOutcome() {
     document.querySelector("#coin-flip").classList.remove("animate");
     let random = Math.round(Math.random());
-    document.querySelector(".user-pick").innerHTML = `The outcome is ${outcomeOptions[random]}`;
-   clearInterval(x);
+    document.querySelector(".coin-outcome").innerHTML = `The outcome is ${outcomeOptions[random]}`;
+    clearInterval(x);
        if (outcomeOptions[random] === "heads") {
         document.querySelector("#coin-flip").style.background = `url("./images/heads.png")`
          playerScore++;
@@ -19,18 +21,19 @@ function heads() {
          computerScore++;
        }
        updateScore();
-  } 
-}              
-
+  }
+}
+/// When player selects tails
 function tails() {
+  document.querySelector(".coin-outcome").innerHTML = "";
   document.querySelector("#coin-flip").classList.add("animate");
   let x = setInterval(randomOutcome, 2000);
-  
+
   function randomOutcome() {
     document.querySelector("#coin-flip").classList.remove("animate");
     let random = Math.round(Math.random());
-    document.querySelector(".user-pick").innerHTML = `The outcome is ${outcomeOptions[random]}`;
-   clearInterval(x);
+    document.querySelector(".coin-outcome").innerHTML = `The outcome is ${outcomeOptions[random]}`;
+    clearInterval(x);
        if (outcomeOptions[random] === "tails") {
         document.querySelector("#coin-flip").style.background = `url("./images/tails.png")`;
         playerScore++;;
@@ -39,11 +42,26 @@ function tails() {
         computerScore++;
        }
        updateScore();
-    }              
+    }
 }
 
 // Updates the scoreboard after coin toss.
 function updateScore() {
   document.querySelector(".player-score").innerHTML = playerScore;
   document.querySelector(".computer-score").innerHTML = computerScore;
+  if (playerScore === 3) {
+    alert("You Won!!!");
+    reset();
+  } else if (computerScore === 3) {
+    alert("The Computer Won");
+    reset();
+  }
+  }
+
+// Reset scoreboard after game ends.
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
+  document.querySelector(".coin-outcome").innerHTML = "";
 }
